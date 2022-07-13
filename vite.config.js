eec5f6cfcf56eaffa005e7c88c705266a2aade5a
jalilverdiyev <calilverdiyev2@gmail.com>
2022-07-14 00:01:19 +0400

    some server changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    build: {
+        manifest: true,
+
+        rollupOptions: {
+            input: '/path/to/main.js'
+        }
+    },
+
     plugins: [
         laravel([
             'resources/css/app.css',
